110a1aa2aa5c1c8abaf73345e09f70a546cade22
viisualworks <dshege4@gmail.com>
2024-04-20 00:39:13 +0300

    replace signle quotes with double quotes

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import * as Accordion from '@radix-ui/react-accordion';
-import Image from 'next/image';
+import * as Accordion from "@radix-ui/react-accordion";
+import Image from "next/image";
 import Plus from "../../public/assets/Plus.svg";
 
 
@@ -40,14 +40,14 @@ const items = [
 export default function Faq(){
 return(
 <div className="flex flex-col w-full py-[48px] lg:py-[60px] lg:flex-row lg:gap-x-6">
-    <div className='lg:w-1/3 lg:py-8 lg:pr-14'>
+    <div className="lg:w-1/3 lg:py-8 lg:pr-14">
         <h3 className="text-[#EB2891] text-[14px] font-medium lg:text-base"> Frequently Asked Questions </h3>
         <h1 className="py-4 text-2xl font-medium text-[#172026] lg:text-[42px] lg:leading-[58px]">Lets clarify some of your questions</h1>
         <p className="text-[#36495C] pb-[24px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore </p>
     </div>
 
-    <div className='lg:w-2/3'>
+    <div className="lg:w-2/3">
         <Accordion.Root
             type="single"
             defaultValue="item-1"
@@ -58,8 +58,8 @@ return(
               <div key={index}>
                     <Accordion.Item value={`item-${index + 1}`} className="bg-[#E3F1FF] p-[16px] rounded-[8px]">
                         <Accordion.Header>
-                            <Accordion.Trigger className='flex w-full items-center justify-between'>
-                                <p className='text-left font-medium text-[#172026] lg:text-[18px]'>{item.question}</p>
+                            <Accordion.Trigger className="flex w-full items-center justify-between">
+                                <p className="text-left font-medium text-[#172026] lg:text-[18px]">{item.question}</p>
                             <span>
                                 <Image src={Plus} alt="" className="h-10 w-10 lg:h-5 lg:w-5" />
                             </span>
@@ -67,7 +67,7 @@ return(
                         </Accordion.Header>
 
                         <Accordion.Content>
-                            <p className='pt-2 text-[#36485C]'>{item.answer}</p>
+                            <p className="pt-2 text-[#36485C]">{item.answer}</p>
                         </Accordion.Content>
                     </Accordion.Item>
               </div>  
