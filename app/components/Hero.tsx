ffff4fdee26afc09a29c7a03262b3fe2612f3844
viisualworks <dshege4@gmail.com>
2024-04-20 00:48:33 +0300

    replace single quite with unicode

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,7 +16,7 @@ return(
             className="text-center text-[32px] leading-[40px] font-bold text-[#172026] lg:text-[64px] lg:leading-[72px]">
             Start monitoring your website
             like a pro</h1>
-        <p className="text-center pt-6 text-[#36485C] lg:text-[18px] lg:leading-7">Get a bird's eye view with our
+        <p className="text-center pt-6 text-[#36485C] lg:text-[18px] lg:leading-7">Get a bird&apos;s eye view with our
             customizable dashboard. Stay on
             top of things! Revamp your work process with
             our game-changing feature. Boost productivity and efficiency!</p>
